db9a2c8a59ca43b6f11a4c719a4769f825924a17
0xlilith <0xlilith@protonmail.com>
2022-08-04 23:48:34 +0530

    add: debug header

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "../lib/common.h"
 #include "../lib/chunk.h"
+#include "../lib/debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    // disassembl chunk
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
