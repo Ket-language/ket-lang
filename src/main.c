53fbd5b095d9d0a3b1f4e28ae97d8e7095e6e426
0xlilith <0xlilith@protonmail.com>
2022-08-04 23:33:23 +0530

    edit: main.c for test

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
 #include "../lib/common.h"
+#include "../lib/chunk.h"
 
 int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
     return 0;
 }
