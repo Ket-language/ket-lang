29cbb478ddffe1f45aa5eaee6e0ed53dd350acee
0xlilith <0xlilith@protonmail.com>
2022-08-04 23:44:55 +0530

    test bin

--- src/chunk.c
+++ src/chunk.c
@@ -14,7 +14,7 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
     if (chunk->capacity < chunk->count +1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
-        chunk->code     = GROW_ARRAY(uint8_t, chunk->code,oldCapacity, chunk->capacity)
+        chunk->code     = GROW_ARRAY(uint8_t, chunk->code,oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
