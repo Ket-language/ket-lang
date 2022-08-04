e12e409d54d9c96ccd77ca52cbd2800df5686e8b
0xlilith <0xlilith@protonmail.com>
2022-08-04 23:56:04 +0530

    add: disassembler to print opcode

---
+++ src/debug.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "../lib/debug.h"
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+    printf("== %s ==\n", name);
+
+    for(int offset = 0; offset < chunk->count;) {
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+int disassembleInstruction(Chunk* chunk, int offset) {
+    printf("%04d ", offset);
+
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            print("Unknown opcode %d\n", instruction);
+            return offset + 1;
+    }
+}
+
+static int simpleInstruction(const char* name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
