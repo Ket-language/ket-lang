6898d369f6dd16b47600d71883ed479229e18c19
0xlilith <0xlilith@protonmail.com>
2022-11-29 18:50:53 +0530

    added: Compiler rules

--- compiler/vm.c
+++ compiler/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "../lib/common.h"
+#include "../lib/compiler.h"
 #include "../lib/debug.h"
 #include "../lib/vm.h"
 
@@ -75,8 +76,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip    = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source)
+    return INTERPRET_OK;
 }
---
+++ lib/compiler.h
@@ -0,0 +1,6 @@
+#ifndef ket_compiler_h
+#define ket_compiler_h
+
+void compile(const char* source);
+
+#endif
